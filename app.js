3dac6e17cb611b1a365d22fcb31723cbe6cd7233
Tina Wulff <tina@tinawulff.dk>
2025-02-06 13:00:12 +0100

    done

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ fetch("https://icanhazdadjoke.com", {       //Object
 .then((response) => response.json())
 .then((data) => {
     console.log(data)
+
+    let divElm = document.querySelector("div");
+    let jokeElm = document.createElement("p");
+    jokeElm.classList.add("joke")
+
+    jokeElm.innerHTML = `${data.joke}`          //data."classList"
+
+    divElm.append(jokeElm);
 })
 
 
